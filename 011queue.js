6c7cb5230cd29d9f3e42401c696200b0d2b46dd1
nori-marcos <marcos.noriyuki.miyata@gmail.com>
2022-06-21 18:46:49 +0000

    implementing queue

---
+++ 011queue.js
@@ -0,0 +1,15 @@
+export default class Queue
+{
+    // Array is used to implement a Queue
+    constructor()
+    {
+        this.items = [];
+    }
+                  
+    // Functions to be implemented
+    // enqueue(item)
+    // dequeue()
+    // front()
+    // isEmpty()
+    // printQueue()
+}
